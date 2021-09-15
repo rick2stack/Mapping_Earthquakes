be76d857b1ac63d7ac493e1005dbfef183e496ee
rick2stack <82985823+rick2stack@users.noreply.github.com>
2021-09-14 20:22:51 -0500

    updated logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,11 +7,11 @@ let map = L.map("mapid", {
     center: [
       40.7, -94.5
     ],
-    zoom: 4
+    zoom: 6
   });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -19,3 +19,17 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// this adds a radius of x amount of meters
+var marker = L.marker([34.0522, -118.2437]).addTo(map);
+var circle= L.circle([34.0522, -118.2437], {
+  color: "yellow",
+  radius: 100
+}).addTo(map);
+
+/// creates a circle/measures a circle. Default size is 10 pixels
+L.circleMarker([34.0522, -118.2437], {
+  radius:100,
+  color: "blue",
+  fillcolor: "#ffffa1"
+}).addTo(map);
